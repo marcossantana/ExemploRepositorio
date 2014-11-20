a95763e2768921ed88acd47426854f3a413a0bf8
marcossantana <marcthe2@gmail.com>
2014-11-20 03:41:44 -0300

    onemore

---
+++ cadastrar.php
@@ -0,0 +1,16 @@
+<?php include_once "denied.php";?>
+
+<?php include_once "top.php";?>
+<?php include_once "menu.php";?>
+
+<body>
+
+<center>
+
+<div id="conteudo">
+<?php include_once "includes/cadastrar.php";?>
+</div><!--conteudo-->
+
+</center>
+
+</body>
